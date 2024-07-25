b0d6295cc11f977b0008a2dc27333e06d3a472ac
System Administrator <root@Darshans-MacBook-Pro-2.local>
2024-07-25 10:14:28 -0700

    Configured Amplify and fixed fs module issue

--- pages/[regular].js
+++ pages/[regular].js
@@ -1,24 +1,21 @@
-import { getSinglePage, getListPage } from '@lib/contentParser';
+import Base from "@layouts/Baseof";
+import NotFound from "@layouts/404";
+import About from "@layouts/About";
+import Contact from "@layouts/Contact";
+import Default from "@layouts/Default";
+import { getSinglePage, getRegularPage } from '@lib/server/contentParser';
 
-// Regular pages
 const RegularPages = ({ data }) => {
   const { title, meta_title, description, image, noindex, canonical, layout } = data.frontmatter;
   const { content } = data;
 
   return (
-    <Base
-      title={title}
-      description={description ? description : content.slice(0, 120)}
-      meta_title={meta_title}
-      image={image}
-      noindex={noindex}
-      canonical={canonical}
-    >
-      {layout === '404' ? (
+    <Base title={title} description={description ? description : content.slice(0, 120)} meta_title={meta_title} image={image} noindex={noindex} canonical={canonical}>
+      {layout === "404" ? (
         <NotFound data={data} />
-      ) : layout === 'about' ? (
+      ) : layout === "about" ? (
         <About data={data} />
-      ) : layout === 'contact' ? (
+      ) : layout === "contact" ? (
         <Contact data={data} />
       ) : (
         <Default data={data} />
@@ -29,13 +26,10 @@ const RegularPages = ({ data }) => {
 
 export default RegularPages;
 
-// for regular page routes
 export const getStaticPaths = async () => {
   const slugs = getSinglePage('content');
   const paths = slugs.map((item) => ({
-    params: {
-      regular: item.slug,
-    },
+    params: { regular: item.slug },
   }));
 
   return {
@@ -44,14 +38,11 @@ export const getStaticPaths = async () => {
   };
 };
 
-// for regular page data
 export const getStaticProps = async ({ params }) => {
   const { regular } = params;
-  const data = getSinglePage('content').find((page) => page.slug === regular);
+  const data = getRegularPage(regular);
 
   return {
-    props: {
-      data,
-    },
+    props: { data },
   };
 };
