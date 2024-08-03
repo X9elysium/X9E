8c3a678726e6dcb590bbe49a8576575fbbcc4923
Darsh Patel <darshanpatel1902@gmail.com>
2024-08-03 11:25:34 -0700

    update json

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ let h4 = h5 * font_scale;
 let h3 = h4 * font_scale;
 let h2 = h3 * font_scale;
 let h1 = h2 * font_scale;
+
 let fontPrimary, fontPrimaryType, fontSecondary, fontSecondaryType;
 if (theme.fonts.font_family.primary) {
   fontPrimary = theme.fonts.font_family.primary
@@ -54,16 +55,16 @@ module.exports = {
         "theme-dark": theme.colors.default.theme_color.theme_dark,
       },
       fontSize: {
-        base: font_base + "px",
-        h1: h1 + "rem",
-        "h1-sm": h1 * 0.8 + "rem",
-        h2: h2 + "rem",
-        "h2-sm": h2 * 0.8 + "rem",
-        h3: h3 + "rem",
-        "h3-sm": h3 * 0.8 + "rem",
-        h4: h4 + "rem",
-        h5: h5 + "rem",
-        h6: h6 + "rem",
+        base: `${font_base}px`,
+        h1: `${h1}rem`,
+        "h1-sm": `${h1 * 0.8}rem`,
+        h2: `${h2}rem`,
+        "h2-sm": `${h2 * 0.8}rem`,
+        h3: `${h3}rem`,
+        "h3-sm": `${h3 * 0.8}rem`,
+        h4: `${h4}rem`,
+        h5: `${h5}rem`,
+        h6: `${h6}rem`,
       },
       fontFamily: {
         primary: [fontPrimary, fontPrimaryType],
