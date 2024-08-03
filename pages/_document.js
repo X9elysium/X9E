a2139d7cfcdbd1051d6b97f6bcd0b8bf87f0d062
Darsh Patel <darshanpatel1902@gmail.com>
2024-08-03 11:05:51 -0700

    added json and script

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import TwSizeIndicator from '@components/TwSizeIndicator';
 import config from '@config/config.json';
 import theme from '@config/theme.json';
-// _document.js
+import Script from 'next/script';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -21,6 +21,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+
+          <Script id="clarity" strategy="beforeInteractive">
+            {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "nhmfksrzgs");`}
+          </Script>
+
+          <Script id="gtm" strategy="afterInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PTM5F7CJ');`}
+          </Script>
+
           <link rel="shortcut icon" href={favicon} />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href={fontUrl} rel="stylesheet" />
