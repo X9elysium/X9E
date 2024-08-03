ec96b47c2d10d7578e259546103a9e56e7ff1f3b
Darsh Patel <darshanpatel1902@gmail.com>
2024-08-03 11:15:20 -0700

    added json and script

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import TwSizeIndicator from '@components/TwSizeIndicator';
 import config from '@config/config.json';
 import theme from '@config/theme.json';
 import Script from 'next/script';
@@ -21,6 +20,16 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <Script id="hotjar" strategy="beforeInteractive">
+            {`(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:5082743,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+          </Script>
 
           <Script id="clarity" strategy="beforeInteractive">
             {`(function(c,l,a,r,i,t,y){
@@ -35,7 +44,7 @@ class MyDocument extends Document {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PTM5F7CJ');`}
+            })(window,document,'script','dataLayer','GTM-PTM5F7CJ');`} {/* Replace 'GTM-PTM5F7CJ' with your GTM ID */}
           </Script>
 
           <link rel="shortcut icon" href={favicon} />
@@ -49,6 +58,16 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PTM5F7CJ"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
         </body>
       </Html>
     );
